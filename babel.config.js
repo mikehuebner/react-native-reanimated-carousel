a81704d054957de2d61f3431fe3dbe74523aafa5
Mike <mikehuebner@me.com>
2024-06-04 15:11:29 -0700

    fix: issue with publishing the correct modules with babel config (#4)

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+export default {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-private-methods',
+    'react-native-reanimated/plugin',
+    '@babel/plugin-syntax-dynamic-import',
+  ],
+};
